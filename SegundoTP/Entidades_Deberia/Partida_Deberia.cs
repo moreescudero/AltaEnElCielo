dee2dc1e5be3145583904df2ea6f3a14f94378dc
moreescudero <escuderomore1@gmail.com>
2022-11-12 01:20:30 -0300

    arreglos

--- SegundoTP/Entidades_Deberia/Partida_Deberia.cs
+++ SegundoTP/Entidades_Deberia/Partida_Deberia.cs
@@ -10,61 +10,11 @@ namespace Entidades_Deberia
     [TestClass]
     public class Partida_Deberia
     {
-        //private List<Carta> EscribirMazo()
-        //{
-        //    List<Carta> mazo = new List<Carta>();
-
-        //    mazo.Add(new Carta(1, "espada", EValores.AnchoEspada));
-        //    mazo.Add(new Carta(2, "espada", EValores.Dos));
-        //    mazo.Add(new Carta(3, "espada", EValores.Tres));
-        //    mazo.Add(new Carta(4, "espada", EValores.Cuatro));
-        //    mazo.Add(new Carta(5, "espada", EValores.Cinco));
-        //    mazo.Add(new Carta(6, "espada", EValores.Seis));
-        //    mazo.Add(new Carta(7, "espada", EValores.SieteEspada));
-        //    mazo.Add(new Carta(10, "espada", EValores.Diez));
-        //    mazo.Add(new Carta(11, "espada", EValores.Once));
-        //    mazo.Add(new Carta(12, "espada", EValores.Doce));
-        //    mazo.Add(new Carta(1,"oro", EValores.Uno));
-        //    mazo.Add(new Carta(2, "oro", EValores.Dos));
-        //    mazo.Add(new Carta(3, "oro", EValores.Tres));
-        //    mazo.Add(new Carta(4, "oro", EValores.Cuatro));
-        //    mazo.Add(new Carta(5, "oro", EValores.Cinco));
-        //    mazo.Add(new Carta(6, "oro", EValores.Seis));
-        //    mazo.Add(new Carta(7, "oro", EValores.SieteOro));
-        //    mazo.Add(new Carta(10, "oro", EValores.Diez));
-        //    mazo.Add(new Carta(11, "oro", EValores.Once));
-        //    mazo.Add(new Carta(12, "oro", EValores.Doce));
-        //    mazo.Add(new Carta(1, "copa", EValores.Uno));
-        //    mazo.Add(new Carta(2, "copa", EValores.Dos));
-        //    mazo.Add(new Carta(3, "copa", EValores.Tres));
-        //    mazo.Add(new Carta(4, "copa", EValores.Cuatro));
-        //    mazo.Add(new Carta(5, "copa", EValores.Cinco));
-        //    mazo.Add(new Carta(6, "copa", EValores.Seis));
-        //    mazo.Add(new Carta(7, "copa", EValores.Siete));
-        //    mazo.Add(new Carta(10, "copa", EValores.Diez));
-        //    mazo.Add(new Carta(11, "copa", EValores.Once));
-        //    mazo.Add(new Carta(12, "copa", EValores.Doce));
-        //    mazo.Add(new Carta(1, "basto", EValores.AnchoBasto));
-        //    mazo.Add(new Carta(2, "basto", EValores.Dos));
-        //    mazo.Add(new Carta(3, "basto", EValores.Tres));
-        //    mazo.Add(new Carta(4, "basto", EValores.Cuatro));
-        //    mazo.Add(new Carta(5, "basto", EValores.Cinco));
-        //    mazo.Add(new Carta(6, "basto", EValores.Seis));
-        //    mazo.Add(new Carta(7, "basto", EValores.Siete));
-        //    mazo.Add(new Carta(10, "basto", EValores.Diez));
-        //    mazo.Add(new Carta(11, "basto", EValores.Once));
-        //    mazo.Add(new Carta(12, "basto", EValores.Doce));
-
-        //    return mazo;
-        //}
 
         [TestMethod]
         public void Barajar_Deberia()
         {
-            //Mazo mazoAux = new Mazo();
-            //mazoAux.Mazos = EscribirMazo();
-            //Serializador<Mazo>.EscribirJSon(mazoAux, "mazo.json");
-            Mazo mazo = Serializador<Mazo>.LeerJSon("mazo.json"); //me tira excepcion por no encontrar el archivo
+            Mazo mazo = Serializador<Mazo>.LeerJSon("mazo.json"); 
             List<Carta> listaMazo = mazo.Mazos;
             Partida partida = new Partida(0);
 
@@ -78,8 +28,14 @@ namespace Entidades_Deberia
         [TestMethod]
         public void Repartir_Deberia()
         {
-            Mazo mazo = Serializador<Mazo>.LeerJSon("mazo.json"); //me tira excepcion por no encontrar el archivo
+            Mazo mazo = Serializador<Mazo>.LeerJSon("mazo.json"); 
             List<Carta> listaMazo = mazo.Mazos;
+            Partida partida = new Partida(0, DevolverJugadoresPrueba(), DateTime.Now);
+
+            partida.Barajar(listaMazo);
+            partida.Repartir(listaMazo);
+
+            Assert.IsTrue(partida.Jugadores[0].Cartas.Count == 3 && partida.Jugadores[1].Cartas.Count == 3);
         }
 
         //[TestMethod] //no se como pasar un objeto para que pueda ser nulo
@@ -88,7 +44,7 @@ namespace Entidades_Deberia
 
         //}
 
-        private List<Usuario> CrearPartidaPrueba()
+        private List<Usuario> DevolverJugadoresPrueba()
         {
             List<Usuario> jugadores = new List<Usuario>()
             {
@@ -104,7 +60,7 @@ namespace Entidades_Deberia
         [TestMethod]
         public void DeterminarGanadorEnvido_DeberiaGanarJug1(int jug1, int jug2)
         {
-            List<Usuario> jugadores = CrearPartidaPrueba();
+            List<Usuario> jugadores = DevolverJugadoresPrueba();
             Partida partida = new Partida(0, jugadores, DateTime.Now);
             jugadores[0].EsMano = true;
 
@@ -124,7 +80,7 @@ namespace Entidades_Deberia
         [TestMethod]
         public void DeterminarGanadorEnvido_DeberiaGanarJug2(int jug1, int jug2)
         {
-            List<Usuario> jugadores = CrearPartidaPrueba();
+            List<Usuario> jugadores = DevolverJugadoresPrueba();
             Partida partida = new Partida(0, jugadores, DateTime.Now);
             jugadores[1].EsMano = true;
 
@@ -142,7 +98,7 @@ namespace Entidades_Deberia
         [TestMethod]
         public void DeterminarGanadorEnvido_Deberia(int jug1, int jug2)
         {
-            List<Usuario> jugadores = CrearPartidaPrueba();
+            List<Usuario> jugadores = DevolverJugadoresPrueba();
             Partida partida = new Partida(0, jugadores, DateTime.Now);
 
             string? ganador = partida.DeterminarGanadorEnvido(jug1, jug2);
@@ -155,7 +111,7 @@ namespace Entidades_Deberia
         [TestMethod]
         public void AsignarTurno_Deberia() // faltan más unit testing pero necesito pasar las cartas como datarow
         {
-            List<Usuario> jugadores = CrearPartidaPrueba();
+            List<Usuario> jugadores = DevolverJugadoresPrueba();
             Partida partida = new Partida(0, jugadores, DateTime.Now);
 
             bool EsTurnoDeJug2 = partida.AsignarTurno();
